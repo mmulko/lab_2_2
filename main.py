cf36dc038653850aaf2fee9fc826f65e5e69ef3f
mmulko <mykhailo.mulko@ucu.edu.ua>
2021-02-15 12:07:02 +0200

    Prepared functions for generating list of films in same year and country

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from geopy.exc import GeocoderTimedOut
 def main(loc_lst):
     year = '(' + input("Enter year: ") + ')'
     point = input("Enter your location: ")
-    file = open('locations.list', 'r')
-    y_list = get_year_list(year, file)
+    location = coordinates_to_address(point)
+    location = location.split(", ")[-1]
+    print(location)
+    file = create_list('locations.list')
+    y_list = get_year_list(year, file, location)
     return y_list
 
 
@@ -20,14 +23,38 @@ def get_coordinates(location: str):
         print("Error: geocode failed on input %s with message %s")
 
 
-def get_year_list(year, file):
-    df = pd.DataFrame(file)
+def create_list(file):
+    f_list = []
+    with open(file) as f:
+        for _ in range(14):
+            f.readline()
+        for line in f:
+            res = line.split()
+            for elem in range(len(res)):
+                res[elem].rstrip("\n")
+            f_list.append(res)
+    return f_list
+
+
+def get_year_list(year, file, loc):
     year_list = []
-    for num in range(14, len(df[0]) - 1):
-        smth = df[0][num].split()
-        if year in smth:
-            year_list.append(smth)
-    return year_list
+    for num in range(14, len(file) - 1):
+        if year in file[num]:
+            year_list.append(file[num])
+    loc_list = []
+    for num in range(len(year_list)):
+        if loc in year_list[num]:
+            loc_list.append(year_list[num])
+    return loc_list
+
+
+def coordinates_to_address(point: str):
+    geolocator = Nominatim(user_agent="mmulko")
+    try:
+        location = geolocator.reverse(point, language='en')
+        return location.address
+    except GeocoderTimedOut as e:
+        print("Error: geocode failed on input %s with message %s")
 
 
 if __name__ == '__main__':
